363a32d5b7c6f236ff5150612b89f871cb5fcef0
황승기 <146107690+Seungkiii@users.noreply.github.com>
2025-07-14 18:06:43 +0900

    second commit

---
+++ codes/second.sh
@@ -0,0 +1,8 @@
+[hwang@localhost ~]$ pwd
+/home/hwang
+[hwang@localhost ~]$ ls
+Open Download Document Desktop Video Photo Format Music
+[hwang@localhost ~]$ cd Music
+[hwang@localhost Music]$ cd ..
+[hwang@localhost ~]$ cd Picture
+[hwang@localhost Picture]$
