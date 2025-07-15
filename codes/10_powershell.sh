be6f3cc3a7a95f118448991628813829457c7177
황승기 <146107690+Seungkiii@users.noreply.github.com>
2025-07-15 12:13:56 +0900

    third commit

---
+++ codes/10_powershell.sh
@@ -0,0 +1,11 @@
+PS C:\Users\Administrator> cd C:\Develops
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14   오후 2:40                Downloads
+d-----      2025-07-14   오후 6:10                VMs
